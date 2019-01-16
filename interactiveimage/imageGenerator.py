4c512edc52040d86cb1605ccf76f12c1e36909bd
sjdlloyd <sjdlloyd@live.co.uk>
2019-01-16 22:07:51 +1300

    implementation of hovering over point

--- interactiveimage/imageGenerator.py
+++ interactiveimage/imageGenerator.py
@@ -1,3 +1,4 @@
+import math
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -7,11 +8,14 @@ data = np.array([[100,100], [500,100], [1000, 400]])
 small_points = ax[0].scatter(data[:,0], data[:,1])
 im = plt.imread('../../../OneDrive/Pictures/IMG_1778.jpg')
 ax[0].imshow(im)
-large_points = ax[0].scatter(data[:, 0], data[:, 1], s=10)
-
-# im = plt.imshow(np.random.rand(10,10)*255, interpolation=
-# fig = plt.gcf()
-# ax = plt.gca()
+plots = [ax[0].scatter([d[0]], [d[1]], s=20) for d in data]
+class PlotThing:
+    """This was created because I could not find the data attribute of a plot"""
+    def __init__(self, plot, data_point): self.plot, self.dp = plot, data_point
+    def compare(self, xdata, ydata): return max(np.abs(self.dp[0] - xdata), np.abs(self.dp[1] - ydata))
+    def __repr__(self): return 'PlotThing({})'.format(self.dp)
+    def set_visible(self, b:bool): self.plot.set_visible(b)
+plot_things = list(map(lambda x: PlotThing(x[0], x[1]), zip(plots, data)))
 
 class EventHandler:
     def __init__(self, data):
@@ -24,7 +28,6 @@ class EventHandler:
             return
         xi, yi = (int(round(n)) for n in (event.xdata, event.ydata))
         value = im[yi,xi]
-        # color = im.cmap(im.norm(value))
         ax[1].scatter(np.ones(10)*1000, np.random.rand(10)*1000)
         print(xi, yi, value)  #,color)
         fig.canvas.draw_idle()
@@ -33,14 +36,14 @@ class EventHandler:
     def on_hover(self, event):
         if event.inaxes!=ax[0]:
             return
-        if self.near_point(event.xdata, event.ydata):
-            large_points.set_visible(True)
-
-        else:
-            large_points.set_visible(False)
-        xi, yi = (int(round(n)) for n in (event.xdata, event.ydata))
+        for d in self.data:
+            d.set_visible(False)
+        # list(map(lambda d: d.set_visible(False), self.data))
+        nearest = self.find_nearest(event.xdata, event.ydata)
+        nearest.set_visible(True)
+        xi, yi = (int(math.floor(n)) for n in (event.xdata, event.ydata))
         value = im[yi,xi]
-        # color = im.cmap(im.norm(value))
+        print([d.plot.get_visible() for d in self.data])
         ax[1].scatter(np.ones(10)*500, np.random.rand(10)*1000)
         print(xi,yi,value)#,color)
         fig.canvas.draw_idle()
@@ -49,6 +52,11 @@ class EventHandler:
         dist_x, dist_y = np.abs(self.data[:,0] - xdata), np.abs(self.data[:,1] - ydata)
         return any(dist_x < 100) and any(dist_y < 100)
 
+    def find_nearest(self, xdata, ydata):
+        sorted_list = sorted(self.data, key=lambda d:d.compare(xdata, ydata))
+        # print(sorted_list)
+        return sorted_list[0]
 
-handler = EventHandler(data)
+
+handler = EventHandler(plot_things)
 plt.show()
